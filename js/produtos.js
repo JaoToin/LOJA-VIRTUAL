a31e5694eaf852149ca2166a210f94accce0d0e4
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-07-06 18:51:39 -0300

    mudanças no arquivo sobre.html, no arquivo sobre.css, carrinho.css e arquivo criado no js

---
+++ js/produtos.js
@@ -0,0 +1,171 @@
+const plantasDoJardim = [
+    // SEÇÃO 1: AROMÁTICAS E MEDICINAIS
+    {
+        id: 1,
+        titulo: "LAVANDA",
+        valor: 25.00,
+        imagem: "../imagens/lavanda.jpg",
+        alt: "flor lavanda",
+        secao: "Aromáticas e Medicinais"
+    },
+    {
+        id: 2,
+        titulo: "ALECRIM",
+        valor: 18.00,
+        imagem: "../imagens/alecrim.jpg",
+        alt: "vaso de alecrim",
+        secao: "Aromáticas e Medicinais"
+    },
+    {
+        id: 3,
+        titulo: "HORTELÃ",
+        valor: 12.00,
+        imagem: "../imagens/hortela.jpg",
+        alt: "hortelã fresca no vaso",
+        secao: "Aromáticas e Medicinais"
+    },
+    {
+        id: 4,
+        titulo: "MANJERICÃO",
+        valor: 15.00,
+        imagem: "../imagens/manjericao.jpg",
+        alt: "maço de manjericão roxo",
+        secao: "Aromáticas e Medicinais"
+    },
+
+    // SEÇÃO 2: FLORES AQUÁTICAS E EXÓTICAS
+    {
+        id: 5,
+        titulo: "LOTUS",
+        valor: 50.00,
+        imagem: "../imagens/lotus.png",
+        alt: "flor de lotus",
+        secao: "Aquáticas e Exóticas"
+    },
+    {
+        id: 6,
+        titulo: "ANTÚRIO",
+        valor: 45.00,
+        imagem: "../imagens/anturio.jpg",
+        alt: "antúrio vermelho em vaso",
+        secao: "Aquáticas e Exóticas"
+    },
+    {
+        id: 7,
+        titulo: "AVE DO PARAÍSO",
+        valor: 60.00,
+        imagem: "../imagens/ave-do-paraiso.jpg",
+        alt: "flor ave do paraíso exótica",
+        secao: "Aquáticas e Exóticas"
+    },
+    {
+        id: 8,
+        titulo: "COPO DE LEITE",
+        valor: 32.00,
+        imagem: "../imagens/copo-de-leite.jpg",
+        alt: "flor copo de leite branca",
+        secao: "Aquáticas e Exóticas"
+    },
+
+    // SEÇÃO 3: FLORES DE VASO TRADICIONAIS
+    {
+        id: 9,
+        titulo: "BEGONIA",
+        valor: 30.00,
+        imagem: "../imagens/begonia.jpg",
+        alt: "flor begonia",
+        secao: "Flores de Vaso"
+    },
+    {
+        id: 10,
+        titulo: "FLOR DE MAIO",
+        valor: 35.00,
+        imagem: "../imagens/flor-de-maio.jpeg",
+        alt: "flor de maio",
+        secao: "Flores de Vaso"
+    },
+    {
+        id: 11,
+        titulo: "CAMELIA",
+        valor: 15.00,
+        imagem: "../imagens/camelias.jpg",
+        alt: "flor camelia",
+        secao: "Flores de Vaso"
+    },
+    {
+        id: 12,
+        titulo: "VIOLETA",
+        valor: 10.00,
+        imagem: "../imagens/violeta.jpg",
+        alt: "violeta roxa pequena em vaso",
+        secao: "Flores de Vaso"
+    },
+
+    // SEÇÃO 4: SUCULENTAS E CACTOS
+    {
+        id: 13,
+        titulo: "ROSA DE PEDRA",
+        valor: 20.00,
+        imagem: "../imagens/rosa-de-pedra.jpg",
+        alt: "suculenta rosa de pedra",
+        secao: "Suculentas e Cactos"
+    },
+    {
+        id: 14,
+        titulo: "CACTO MANDACARU",
+        valor: 40.00,
+        imagem: "../imagens/mandacaru.jpg",
+        alt: "cacto mandacaru ornamental",
+        secao: "Suculentas e Cactos"
+    },
+    {
+        id: 15,
+        titulo: "BABOSA (ALOE VERA)",
+        valor: 22.00,
+        imagem: "../imagens/babosa.jpg",
+        alt: "planta babosa aloe vera",
+        secao: "Suculentas e Cactos"
+    },
+    {
+        id: 16,
+        titulo: "ORELHA DE SHREK",
+        valor: 18.00,
+        imagem: "../imagens/orelha-shrek.jpg",
+        alt: "suculenta orelha de shrek",
+        secao: "Suculentas e Cactos"
+    },
+
+    // SEÇÃO 5: ORQUÍDEAS E FLORES NOBRES
+    {
+        id: 17,
+        titulo: "ORQUÍDEA PHALAENOPSIS",
+        valor: 75.00,
+        imagem: "../imagens/orquidea-phalaenopsis.jpg",
+        alt: "orquidea phalaenopsis branca",
+        secao: "Orquídeas e Flores Nobres"
+    },
+    {
+        id: 18,
+        titulo: "ORQUÍDEA CHUVA DE OURO",
+        valor: 65.00,
+        imagem: "../imagens/orquidea-chuva-ouro.jpg",
+        alt: "orquidea chuva de ouro amarela",
+        secao: "Orquídeas e Flores Nobres"
+    },
+    {
+        id: 19,
+        titulo: "GIRASSOL",
+        valor: 28.00,
+        imagem: "../imagens/girassol.jpg",
+        alt: "vaso com girassol ornamental",
+        secao: "Orquídeas e Flores Nobres"
+    },
+    {
+        id: 20,
+        titulo: "TULIPA",
+        valor: 55.00,
+        imagem: "../imagens/tulipa.jpg",
+        alt: "tulipa vermelha importada",
+        secao: "Orquídeas e Flores Nobres"
+    }
+];
