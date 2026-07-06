a4da204e29d0056fb9699aa9b7057cf7c82b9299
joao <joao.a.guedes@aluno.senai.br>
2026-07-06 20:40:46 -0300

    feito a ligação dos elem do array junto dos cards do index

--- js/carrega_produtos.js
+++ js/carrega_produtos.js
@@ -2,7 +2,7 @@ import { plantasDoJardim } from "./produtos.js";
 
 //PEGANDO ELEMENTOS DO DOM
 
-const section_cards = document.querySelector(#cards)
+const section_cards = document.querySelector('#cards')
 
 
 const listaProdutos = ()=>{
@@ -22,8 +22,18 @@ const listaProdutos = ()=>{
         const h2titulo = document.createElement('h2')
         h2titulo.innerHTML = elem.titulo
 
-        const divValor = document.createElement('h3')
-        divValor.setAttribute('class','')
+        const h3Valor = document.createElement('h3')
+        h3Valor.setAttribute('class','valor_card')
+        h3Valor.innerHTML `R$ ${parseFloat(elem.valor).toFixed(2).replace('.',',')}`
+
+        const bntCard = document.createElement('button')
+        bntCard.setAttribute('class','bnt_card')
+        bntCard.innerHTML = 'Adicionar'
+
+        divCrad.appendChild(imgProduto)
+        divCrad.appendChild(h2titulo)
+        divCrad.appendChild(h3Valor)
+        divCrad.appendChild(bntCard)
 
     })
 }
