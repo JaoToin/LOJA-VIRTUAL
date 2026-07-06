16d6ff5032d3dda0b9165453e8185eca3bcb0130
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-07-06 19:32:28 -0300

    configurando e importando o array no carrega produtos js

---
+++ js/carrega_produtos.js
@@ -0,0 +1,29 @@
+import { plantasDoJardim } from "./produtos.js";
+
+//PEGANDO ELEMENTOS DO DOM
+
+const section_cards = document.querySelector(#cards)
+
+
+const listaProdutos = ()=>{
+
+    section_cards.innerHTML = ''
+
+    plantasDoJardim.foreach((elem, i)=>{
+
+        const divCrad = document.createElement('div')
+        divCrad.setAttribute('class','card')
+
+        const imgProduto = document.createElement('img')
+        imgProduto.setAttribute('src',elem.imagem)
+        imgProduto.setAttribute('alt',elem.alt)
+        imgProduto.setAttribute('class','img_card')
+
+        const h2titulo = document.createElement('h2')
+        h2titulo.innerHTML = elem.titulo
+
+        const divValor = document.createElement('h3')
+        divCrad.setAttribute('class','')
+
+    })
+}
--- js/produtos.js
+++ js/produtos.js
@@ -169,3 +169,5 @@ const plantasDoJardim = [
         secao: "Orquídeas e Flores Nobres"
     }
 ];
+
+export{plantasDoJardim}
