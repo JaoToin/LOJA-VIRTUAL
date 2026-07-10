66c88a3bd454030a8f71ff1218d5555607421b27
JOAO <JOAO.A.GUEDES@ALUNO.SENAI.BR>
2026-07-09 21:42:44 -0300

    produtos 02, com todos os dados foi criado, so saber oque esta fazendo

--- js/carrega_produtos.js
+++ js/carrega_produtos.js
@@ -8,36 +8,6 @@ const section_cards = document.querySelector('#cards')
 const listaProdutos = ()=>{
 
     section_cards.innerHTML = ''
-
-    plantasDoJardim.forEach((elem, i)=>{
-
-        const divCrad = document.createElement('div')
-        divCrad.setAttribute('class','card')
-
-        const imgProduto = document.createElement('img')
-        imgProduto.setAttribute('src',elem.imagem)
-        imgProduto.setAttribute('alt',elem.alt)
-        imgProduto.setAttribute('class','img_card')
-
-        const h2titulo = document.createElement('h2')
-        h2titulo.innerHTML = elem.titulo
-
-        const h3Valor = document.createElement('h3')
-        h3Valor.setAttribute('class','valor_card')
-        h3Valor.innerHTML = `R$ ${parseFloat(elem.valor).toFixed(2).replace('.',',')}`
-
-        const bntCard = document.createElement('button')
-        bntCard.setAttribute('class','bnt_card')
-        bntCard.innerHTML = 'Adicionar'
-
-        divCrad.appendChild(imgProduto)
-        divCrad.appendChild(h2titulo)
-        divCrad.appendChild(h3Valor)
-        divCrad.appendChild(bntCard)
-
-        section_cards.appendChild(divCrad)
-
-    })
    
 }
 
@@ -144,4 +114,7 @@ const montandoCards = (objProdutos) =>{
     })
 
 }
+
+montandoCards(plantasDoJardim); 
+montarSecoes();
 
