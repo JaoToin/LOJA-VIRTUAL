7e2871af4082563d458b30a4400f6f5893e6e4e7
joao <jguedes2408@gmail.com>
2026-07-17 14:04:45 -0300

    inicio do estudo para a revisão do codigo

--- js/produtos.js
+++ js/produtos.js
@@ -1,5 +1,6 @@
 const plantasDoJardim = [
 
+
     // SEÇÃO 1: AROMÁTICAS E MEDICINAIS
     {
         id: 1,
@@ -37,10 +38,64 @@ const plantasDoJardim = [
         id_secao: 1,
         secao: "Aromáticas e Medicinais"
     },
+    {
+        id: 5,
+        titulo: "CAMOMILA",
+        valor: 14.00,
+        imagem: "../imagens/camomila.png",
+        alt: "flores de camomila no vaso",
+        id_secao: 1,
+        secao: "Aromáticas e Medicinais"
+    },
+    {
+        id: 6,
+        titulo: "CAPIM-LIMÃO",
+        valor: 16.00,
+        imagem: "../imagens/capim-limao.png",
+        alt: "maço de capim-limão brotando",
+        id_secao: 1,
+        secao: "Aromáticas e Medicinais"
+    },
+    {
+        id: 7,
+        titulo: "ERVA-CIDREIRA",
+        valor: 13.00,
+        imagem: "../imagens/erva-cidreira.png",
+        alt: "folhas verdes de erva-cidreira",
+        id_secao: 1,
+        secao: "Aromáticas e Medicinais"
+    },
+    {
+        id: 8,
+        titulo: "TOMILHO",
+        valor: 17.00,
+        imagem: "../imagens/tomilho.png",
+        alt: "ramo de tomilho fresco em vasinho",
+        id_secao: 1,
+        secao: "Aromáticas e Medicinais"
+    },
+    {
+        id: 9,
+        titulo: "SÁLVIA",
+        valor: 19.00,
+        imagem: "../imagens/salvia.png",
+        alt: "folhas de sálvia medicinal",
+        id_secao: 1,
+        secao: "Aromáticas e Medicinais"
+    },
+    {
+        id: 10,
+        titulo: "POEJO",
+        valor: 11.00,
+        imagem: "../imagens/poejo.png",
+        alt: "planta poejo aromática",
+        id_secao: 1,
+        secao: "Aromáticas e Medicinais"
+    },
 
     // SEÇÃO 2: FLORES AQUÁTICAS E EXÓTICAS
     {
-        id: 5,
+        id: 11,
         titulo: "LOTUS",
         valor: 50.00,
         imagem: "../imagens/lotus.png",
@@ -49,7 +104,7 @@ const plantasDoJardim = [
         secao: "Aquáticas e Exóticas"
     },
     {
-        id: 6,
+        id: 12,
         titulo: "ANTÚRIO",
         valor: 45.00,
         imagem: "../imagens/anturio.png",
@@ -58,7 +113,7 @@ const plantasDoJardim = [
         secao: "Aquáticas e Exóticas"
     },
     {
-        id: 7,
+        id: 13,
         titulo: "AVE DO PARAÍSO",
         valor: 60.00,
         imagem: "../imagens/ave-do-paraiso.png",
@@ -67,7 +122,7 @@ const plantasDoJardim = [
         secao: "Aquáticas e Exóticas"
     },
     {
-        id: 8,
+        id: 14,
         titulo: "COPO DE LEITE",
         valor: 32.00,
         imagem: "../imagens/copo-de-leite.png",
@@ -75,10 +130,64 @@ const plantasDoJardim = [
         id_secao: 2,
         secao: "Aquáticas e Exóticas"
     },
+    {
+        id: 15,
+        titulo: "VITÓRIA-RÉGIA",
+        valor: 95.00,
+        imagem: "../imagens/vitoria-regia.png",
+        alt: "grande folha de vitória-régia com flor",
+        id_secao: 2,
+        secao: "Aquáticas e Exóticas"
+    },
+    {
+        id: 16,
+        titulo: "AGUAPÉ",
+        valor: 28.00,
+        imagem: "../imagens/aguape.png",
+        alt: "flor de aguapé roxa flutuante",
+        id_secao: 2,
+        secao: "Aquáticas e Exóticas"
+    },
+    {
+        id: 17,
+        titulo: "NENÚFAR",
+        valor: 48.00,
+        imagem: "../imagens/nenufar.png",
+        alt: "flor de nenúfar rosa na água",
+        id_secao: 2,
+        secao: "Aquáticas e Exóticas"
+    },
+    {
+        id: 18,
+        titulo: "PROTEIA",
+        valor: 70.00,
+        imagem: "../imagens/proteia.png",
+        alt: "flor proteia exótica gigante",
+        id_secao: 2,
+        secao: "Aquáticas e Exóticas"
+    },
+    {
+        id: 19,
+        titulo: "JACINTO D'ÁGUA",
+        valor: 26.00,
+        imagem: "../imagens/jacinto-dagua.png",
+        alt: "jacinto d'água florido",
+        id_secao: 2,
+        secao: "Aquáticas e Exóticas"
+    },
+    {
+        id: 20,
+        titulo: "ALOCASIA",
+        valor: 65.00,
+        imagem: "../imagens/alocasia.png",
+        alt: "folha exótica de alocasia sanderiana",
+        id_secao: 2,
+        secao: "Aquáticas e Exóticas"
+    },
 
     // SEÇÃO 3: FLORES DE VASO TRADICIONAIS
     {
-        id: 9,
+        id: 21,
         titulo: "BEGONIA",
         valor: 30.00,
         imagem: "../imagens/begonia.jpg",
@@ -87,7 +196,7 @@ const plantasDoJardim = [
         secao: "Flores de Vaso"
     },
     {
-        id: 10,
+        id: 22,
         titulo: "FLOR DE MAIO",
         valor: 35.00,
         imagem: "../imagens/flor-de-maio.jpeg",
@@ -96,7 +205,7 @@ const plantasDoJardim = [
         secao: "Flores de Vaso"
     },
     {
-        id: 11,
+        id: 23,
         titulo: "CAMELIA",
         valor: 15.00,
         imagem: "../imagens/camelias.jpg",
@@ -105,7 +214,7 @@ const plantasDoJardim = [
         secao: "Flores de Vaso"
     },
     {
-        id: 12,
+        id: 24,
         titulo: "VIOLETA",
         valor: 10.00,
         imagem: "../imagens/violeta.png",
@@ -113,10 +222,64 @@ const plantasDoJardim = [
         id_secao: 3,
         secao: "Flores de Vaso"
     },
+    {
+        id: 25,
+        titulo: "GERÂNIO",
+        valor: 22.00,
+        imagem: "../imagens/geranio.png",
+        alt: "gerânio vermelho em vaso",
+        id_secao: 3,
+        secao: "Flores de Vaso"
+    },
+    {
+        id: 26,
+        titulo: "AZALEIA",
+        valor: 28.00,
+        imagem: "../imagens/azaleia.png",
+        alt: "mini azaleia rosa florida",
+        id_secao: 3,
+        secao: "Flores de Vaso"
+    },
+    {
+        id: 27,
+        titulo: "CRISÂNTEMO",
+        valor: 24.00,
+        imagem: "../imagens/crisantemo.png",
+        alt: "vaso de crisântemo amarelo",
+        id_secao: 3,
+        secao: "Flores de Vaso"
+    },
+    {
+        id: 28,
+        titulo: "KALANCHOE",
+        valor: 18.00,
+        imagem: "../imagens/kalanchoe.png",
+        alt: "kalanchoe florida colorida",
+        id_secao: 3,
+        secao: "Flores de Vaso"
+    },
+    {
+        id: 29,
+        titulo: "PETÚNIA",
+        valor: 20.00,
+        imagem: "../imagens/petunia.png",
+        alt: "petúnia roxa em vaso suspenso",
+        id_secao: 3,
+        secao: "Flores de Vaso"
+    },
+    {
+        id: 30,
+        titulo: "PRÍMULA",
+        valor: 25.00,
+        imagem: "../imagens/primula.png",
+        alt: "flor prímula colorida em vaso",
+        id_secao: 3,
+        secao: "Flores de Vaso"
+    },
 
     // SEÇÃO 4: SUCULENTAS E CACTOS
     {
-        id: 13,
+        id: 31,
         titulo: "ROSA DE PEDRA",
         valor: 20.00,
         imagem: "../imagens/rosa-de-pedra.png",
@@ -125,7 +288,7 @@ const plantasDoJardim = [
         secao: "Suculentas e Cactos"
     },
     {
-        id: 14,
+        id: 32,
         titulo: "CACTO MANDACARU",
         valor: 40.00,
         imagem: "../imagens/mandacaru.png",
@@ -134,7 +297,7 @@ const plantasDoJardim = [
         secao: "Suculentas e Cactos"
     },
     {
-        id: 15,
+        id: 33,
         titulo: "BABOSA (ALOE VERA)",
         valor: 22.00,
         imagem: "../imagens/babosa.png",
@@ -143,7 +306,7 @@ const plantasDoJardim = [
         secao: "Suculentas e Cactos"
     },
     {
-        id: 16,
+        id: 34,
         titulo: "ORELHA DE SHREK",
         valor: 18.00,
         imagem: "../imagens/orelha-shrek.png",
@@ -151,10 +314,64 @@ const plantasDoJardim = [
         id_secao: 4,
         secao: "Suculentas e Cactos"
     },
+    {
+        id: 35,
+        titulo: "RABO DE MACACO",
+        valor: 45.00,
+        imagem: "../imagens/rabo-de-macaco.png",
+        alt: "cacto rabo de macaco pendente",
+        id_secao: 4,
+        secao: "Suculentas e Cactos"
+    },
+    {
+        id: 36,
+        titulo: "COROA DE FRADE",
+        valor: 35.00,
+        imagem: "../imagens/coroa-de-frade.png",
+        alt: "cacto coroa de frade com copa vermelha",
+        id_secao: 4,
+        secao: "Suculentas e Cactos"
+    },
+    {
+        id: 37,
+        titulo: "PLANTA FANTASMA",
+        valor: 25.00,
+        imagem: "../imagens/planta-fantasma.png",
+        alt: "suculenta planta fantasma azulada",
+        id_secao: 4,
+        secao: "Suculentas e Cactos"
+    },
+    {
+        id: 38,
+        titulo: "COLAR DE PÉROLAS",
+        valor: 38.00,
+        imagem: "../imagens/colar-de-perolas.png",
+        alt: "suculenta colar de pérolas pendente",
+        id_secao: 4,
+        secao: "Suculentas e Cactos"
+    },
+    {
+        id: 39,
+        titulo: "DEDINHO DE MOÇA",
+        valor: 24.00,
+        imagem: "../imagens/dedinho-de-moca.png",
+        alt: "suculenta dedinho de moça cheia",
+        id_secao: 4,
+        secao: "Suculentas e Cactos"
+    },
+    {
+        id: 40,
+        titulo: "CASTELO DE FADA",
+        valor: 30.00,
+        imagem: "../imagens/castelo-de-fada.png",
+        alt: "mini cacto castelo de fada",
+        id_secao: 4,
+        secao: "Suculentas e Cactos"
+    },
 
     // SEÇÃO 5: ORQUÍDEAS E FLORES NOBRES
     {
-        id: 17,
+        id: 41,
         titulo: "ORQUÍDEA PHALAENOPSIS",
         valor: 75.00,
         imagem: "../imagens/orquidea-phalaenopsis.png",
@@ -163,7 +380,7 @@ const plantasDoJardim = [
         secao: "Orquídeas e Flores Nobres"
     },
     {
-        id: 18,
+        id: 42,
         titulo: "ORQUÍDEA CHUVA DE OURO",
         valor: 65.00,
         imagem: "../imagens/orquidea-chuva-ouro.png",
@@ -172,7 +389,7 @@ const plantasDoJardim = [
         secao: "Orquídeas e Flores Nobres"
     },
     {
-        id: 19,
+        id: 43,
         titulo: "GIRASSOL",
         valor: 28.00,
         imagem: "../imagens/girassol.png",
@@ -181,14 +398,69 @@ const plantasDoJardim = [
         secao: "Orquídeas e Flores Nobres"
     },
     {
-        id: 20,
+        id: 44,
         titulo: "TULIPA",
         valor: 55.00,
         imagem: "../imagens/tulipa.png",
         alt: "tulipa vermelha importada",
         id_secao: 5,
         secao: "Orquídeas e Flores Nobres"
+    },
+    {
+        id: 45,
+        titulo: "ROSA COLOMBIANA",
+        valor: 80.00,
+        imagem: "../imagens/rosa-colombiana.png",
+        alt: "botão de rosa colombiana vermelha nobre",
+        id_secao: 5,
+        secao: "Orquídeas e Flores Nobres"
+    },
+    {
+        id: 46,
+        titulo: "PEÔNIA",
+        valor: 90.00,
+        imagem: "../imagens/peonia.png",
+        alt: "flor de peônia rosa bem cheia",
+        id_secao: 5,
+        secao: "Orquídeas e Flores Nobres"
+    },
+    {
+        id: 47,
+        titulo: "HORTÊNSIA",
+        valor: 48.00,
+        imagem: "../imagens/hortensia.png",
+        alt: "buquê de hortênsia azul",
+        id_secao: 5,
+        secao: "Orquídeas e Flores Nobres"
+    },
+    {
+        id: 48,
+        titulo: "LÍRIO DA PAZ",
+        valor: 35.00,
+        imagem: "../imagens/lirio-da-paz.png",
+        alt: "lírio da paz em vaso elegante",
+        id_secao: 5,
+        secao: "Orquídeas e Flores Nobres"
+    },
+    {
+        id: 49,
+        titulo: "MAGNÓLIA",
+        valor: 70.00,
+        imagem: "../imagens/magnolia.png",
+        alt: "flor nobre de magnólia branca",
+        id_secao: 5,
+        secao: "Orquídeas e Flores Nobres"
+    },
+    {
+        id: 50,
+        titulo: "ORQUÍDEA NEGRA",
+        valor: 120.00,
+        imagem: "../imagens/orquidea-negra.png",
+        alt: "rara orquídea negra exótica",
+        id_secao: 5,
+        secao: "Orquídeas e Flores Nobres"
     }
 ];
+
 
 export { plantasDoJardim }
