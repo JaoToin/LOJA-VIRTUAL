bac05dba9dcffeab37bcddd81cbd6e8353ab4ba3
joao <joao.a.guedes@aluno.senai.br>
2026-07-17 20:42:28 -0300

    finalizado o cep oculto

--- js/consulta_cep.js
+++ js/consulta_cep.js
@@ -1,25 +1,26 @@
 //PEGANDO  O INPUT CEP DO DOM 
 const inputCep = document.querySelector('#cep')
+const formPessoa = document.querySelector('#form-pessoa')
 
 //CAPTURANDO O EVENTO AO PERDERR O FOCO 
 inputCep.addEventListener('change', (evt) => {
 
     //PEGANDO OS NUM DO INPUT NÃO 
-    const numCep = evt.target.value.replace(/\D/g,'')
+    const numCep = evt.target.value.replace(/\D/g, '')
 
-    if(numCep.length != 8){
+    if (numCep.length != 8) {
         alert('CEP INVÁLIDO !!!')
         return
-        }
+    }
 
-        buscaDadosCep(numCep)
+    buscaDadosCep(numCep)
 })
 
 //BUSCAR OS DADOS DOS CEP NO VIA CEP 
 
-const buscaDadosCep = async (cep) =>{
+const buscaDadosCep = async (cep) => {
     //TENTAR BUSCAR OS DADOS 
-    try{
+    try {
         //BUSCAR OS DADOS NO VIA CEP
         const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
 
@@ -30,30 +31,30 @@ const buscaDadosCep = async (cep) =>{
         exibeDados(dadosEndereco)
 
         //CASO HAJA ALGUM ERRO É CAPTURADO PELO catch
-    } catch(erro){
-        console.log('ERRO APRESENTADO',erro.message)
+    } catch (erro) {
+        console.log('ERRO APRESENTADO', erro.message)
     }
 }
 
 //OBJETO LITERAL CAMPOS QUE CADA CHAVE REPRESENTA OS INPUTS DO DOM
 const campos = {
     logradouro: document.querySelector('#logradouro'),
-    bairro:  document.querySelector('bairro'),
-    localidade: document.querySelector('localidade'),
-    uf: document.querySelector('uf')
+    bairro: document.querySelector('#bairro'),
+    localidade: document.querySelector('#localidade'),
+    uf: document.querySelector('#uf')
 }
 
 //FUNÇÃO EXIBE DADOS 
 const exibeDados = (objDados) => {
 
+    console.log(objDados)
+
     //PEGANDO A DIV PAI DOS ELEMENTOS DO ENDEREÇO
-    const divEndereco = document.querySelector('#div-dadosendereco')
-
-
+    const divEndereco = document.querySelector('#div-dados-endereco')
     //REMOVE DA DIV O CLASS OCULTO
     divEndereco.classList.remove('oculto')
 
-    document.querySelector('#logradouro').value = objDados.logradouro
+   /* document.querySelector('#logradouro').value = objDados.logradouro
     document.querySelector('#logradouro').disabled = true
 
     document.querySelector('#bairro').value = objDados.bairro
@@ -63,13 +64,28 @@ const exibeDados = (objDados) => {
     document.querySelector('#localidade').disabled = true
 
     document.querySelector('#uf').value = objDados.uf
-    document.querySelector('#uf').disabled = true
+    document.querySelector('#uf').disabled = true*/
 
 
-/*
-    for (let chave in objDados)
+    
+        for (let chave in campos){
+        
+    
+            //atribuir o valor ao input 
+        campos[chave].value = objDados[chave]
+    
+        //bloqueia os inputss. não permite que o usuário apague os valores 
+        campos[chave].disabled = objDados[chave]
+        }
 
-    campos[chave].value = objDados[chave]
-
-    campos[chave].disabled = objDados[chave]*/
+        document.querySelector('#num-residencial').focus()
 } 
+
+formPessoa.addEventListener('reset', () =>{
+
+    //pega a div pai dos elementos do endereço
+    const divEndereco = document.querySelector('#div-dados-endereco')
+
+    //remove da div o class oculto 
+    divEndereco.classList.add('oculto')
+})
